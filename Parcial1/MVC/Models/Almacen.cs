84685a99ef767cd4d856fcbe77777ce22daca922
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-04-06 00:38:13 -0400

    Commit Modelo MVC

---
+++ Parcial1/MVC/Models/Almacen.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public enum TipoProducto
+    {
+        Bebible,
+        Comestible,
+        Liquido
+    }
+
+    public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+    }
+
+
+    public class Almacen
+    {
+        [Key]
+        public int IdProducto { get; set; }
+
+        [Required]
+
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+        [Required]
+        public TipoProducto Producto { get; set; }
+
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+    }
+}
