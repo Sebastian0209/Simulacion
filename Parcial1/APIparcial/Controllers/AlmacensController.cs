8b4ecc2e652aa25915182edfb48907379039388f
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-04-06 00:50:48 -0400

    Commit Web Api

---
+++ Parcial1/APIparcial/Controllers/AlmacensController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using APIparcial.Models;
+
+namespace APIparcial.Controllers
+{
+    public class AlmacensController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Almacens
+        public IQueryable<Almacen> GetAlmacens()
+        {
+            return db.Almacens;
+        }
+
+        // GET: api/Almacens/5
+        [ResponseType(typeof(Almacen))]
+        public IHttpActionResult GetAlmacen(int id)
+        {
+            Almacen almacen = db.Almacens.Find(id);
+            if (almacen == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(almacen);
+        }
+
+        // PUT: api/Almacens/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAlmacen(int id, Almacen almacen)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != almacen.IdProducto)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(almacen).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlmacenExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Almacens
+        [ResponseType(typeof(Almacen))]
+        public IHttpActionResult PostAlmacen(Almacen almacen)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Almacens.Add(almacen);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = almacen.IdProducto }, almacen);
+        }
+
+        // DELETE: api/Almacens/5
+        [ResponseType(typeof(Almacen))]
+        public IHttpActionResult DeleteAlmacen(int id)
+        {
+            Almacen almacen = db.Almacens.Find(id);
+            if (almacen == null)
+            {
+                return NotFound();
+            }
+
+            db.Almacens.Remove(almacen);
+            db.SaveChanges();
+
+            return Ok(almacen);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AlmacenExists(int id)
+        {
+            return db.Almacens.Count(e => e.IdProducto == id) > 0;
+        }
+    }
+}
---
+++ Parcial1/MVC/Controllers/AlmacensController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class AlmacensController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Almacens
+        public ActionResult Index()
+        {
+            return View(db.Almacens.ToList());
+        }
+
+        // GET: Almacens/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacens.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacens/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Almacens/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdProducto,Nombre,Precio,Producto,Estado")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Almacens.Add(almacen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(almacen);
+        }
+
+        // GET: Almacens/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacens.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacens/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdProducto,Nombre,Precio,Producto,Estado")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(almacen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacens/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacens.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Almacen almacen = db.Almacens.Find(id);
+            db.Almacens.Remove(almacen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
